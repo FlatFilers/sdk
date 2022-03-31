8f827bb467aafe5945d18fa60bdd0e1abcaf1179
Jhonatan Uribe <jotauribe@Jhonatans-MacBook-Pro-2.local>
2022-03-31 16:52:45 -0500

    fix: adding name to request error

--- src/errors/RequestError.ts
+++ src/errors/RequestError.ts
@@ -4,4 +4,5 @@ export class RequestError extends FlatfileError {
   public code = 'FF-RE-00'
   public debug = ''
   public userMessage = 'A network request failed to complete. Please try again.'
+  public name = 'RequestError'
 }
