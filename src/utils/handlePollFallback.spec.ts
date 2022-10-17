99482b9c8873adf0a28b45424c9240ba41936205
Roberto Alcantara <cicero.roberto@flatfile.io>
2022-10-17 14:50:55 -0300

    feat(sdk): poll until batch submitted or cancelled

--- src/utils/handlePollFallback.spec.ts
+++ src/utils/handlePollFallback.spec.ts
@@ -4,7 +4,9 @@ const wait = async (time = 0) => await new Promise((resolve) => setTimeout(resol
 
 describe('handlePollFallback', () => {
   test('should execute poll method and callback', async () => {
-    const fallbackMethod = jest.fn().mockResolvedValue(Promise.resolve({ result: true }))
+    const fallbackMethod = jest
+      .fn()
+      .mockResolvedValue(Promise.resolve({ result: true, stopPoll: true }))
     const callback = jest.fn()
 
     handlePollFallback(fallbackMethod, callback, 0, 0)
