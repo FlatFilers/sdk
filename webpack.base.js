eb3fc168831c17aa17bb1b69fb626a24a669d467
David Boskovic <david@flatfile.com>
2022-11-18 12:50:38 -0500

    fix: correct the target

--- webpack.base.js
+++ webpack.base.js
@@ -44,9 +44,7 @@ module.exports = {
               [
                 '@babel/preset-env',
                 {
-                  targets: {
-                    node: 'current',
-                  },
+                  targets: "> 0.25%, not dead",
                 },
               ],
               [
